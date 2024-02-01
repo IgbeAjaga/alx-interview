9be3ca40e02f8a27e1cac93734a0ad795ca21f68
IgbeAjaga <ajagaigbe144@gmail.com>
2024-02-01 11:38:45 -0800

    Making Change

---
+++ 0x08-making_change/0-making_change.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+"""
+Module for making change
+"""
+
+
+def makeChange(coins, total):
+    """
+    Determines the fewest number of coins needed to meet a given amount total.
+
+    Args:
+        coins (list): List of coin values.
+        total (int): The amount total to meet.
+
+    Returns:
+        int: Fewest number of coins needed to meet total.
+             If total is 0 or less, return 0.
+             If total cannot be met by any number of coins, return -1.
+    """
+    if total <= 0:
+        return 0
+
+    dp = [float('inf')] * (total + 1)
+    dp[0] = 0
+
+    for coin in coins:
+        for i in range(coin, total + 1):
+            dp[i] = min(dp[i], dp[i - coin] + 1)
+
+    return dp[total] if dp[total] != float('inf') else -1
+
+
+if __name__ == "__main__":
+    coins1 = [1, 2, 25]
+    total1 = 37
+    print(makeChange(coins1, total1))
+
+    coins2 = [1256, 54, 48, 16, 102]
+    total2 = 1453
+    print(makeChange(coins2, total2))
